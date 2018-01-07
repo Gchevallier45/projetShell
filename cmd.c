9cd68ba625dde0fade6f54d87513a943e12b7dbb
Gchevallier45 <34574840+Gchevallier45@users.noreply.github.com>
2018-01-07 14:59:36 +0100

    Add files via upload

---
+++ cmd.c
@@ -0,0 +1,16 @@
+#include "cmd.h"
+
+//Prints the command
+void printCmd(cmd *cmd){
+	//your implementation comes here
+}
+
+//Initializes the initial_cmd, membres_cmd et nb_membres fields
+void parseMembers(char *inputString,cmd *cmd){
+    //Your implementation comes here
+}
+
+//Frees memory associated to a cmd
+void freeCmd(cmd  * cmd){
+  
+}
